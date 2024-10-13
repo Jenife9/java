03aa5038da068a74b019e6e1e015124968be4f92
Jenife9 <jenifer8471@gmail.com>
2024-10-13 00:21:49 -0600

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,13 @@
+export class Product {
+  /**
+   *
+   * @param {string} names 
+   * @param {number} price 
+   * @param {number} year 
+   */
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
